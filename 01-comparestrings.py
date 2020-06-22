c91264c5397308ebcf4ccbd11fa6f7865c386144
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2020-06-22 12:59:50 +0200

    commit

--- 01-comparestrings.py
+++ 01-comparestrings.py
@@ -4,4 +4,4 @@ str2 = input("Introduce el segundo string: ")
 if str1 == str2:
     print("Ambos strings son iguales")
 else:
-    print("Los dos strings son diferentes")
+    print("Los strings son diferentes")
