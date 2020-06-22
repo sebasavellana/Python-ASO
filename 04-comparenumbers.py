ac7bc435cd7c67f5565cdfd3faf620314e0113b4
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2020-06-22 13:29:55 +0200

    Update 04-comparenumbers.py

--- 04-comparenumbers.py
+++ 04-comparenumbers.py
@@ -1,4 +1,4 @@
-ent1 = input("Introduce un número: ")
+""" ent1 = input("Introduce un número: ")
 ent2 = input("Introduce otro número: ")
 
 if ent1.isnumeric() == True and ent2.isnumeric() == True:
@@ -11,4 +11,16 @@ if ent1.isnumeric() == True and ent2.isnumeric() == True:
     else:
         print(ent2,"es mayor")
 else:
+    print("Debes introducir un número") """
+
+try:
+    ent1 = int(input("Introduce un número: "))
+    ent2 = int(input("Introduce otro número: "))
+    if ent1 == ent2:
+        print("Son iguales")
+    elif ent1 > ent2:
+        print(ent1,"es mayor")
+    else:
+        print(ent2,"es mayor")
+except ValueError:
     print("Debes introducir un número")
