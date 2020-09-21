cb9d7c4cafa4a1d4956f5c1085583bd10033d75a
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2020-09-21 23:40:44 +0200

    Commit

--- ej08.py
+++ ej08.py
@@ -5,3 +5,17 @@
 # 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
 # Teniendo en cuenta que 0! = 1 el script deberá comprobar que el usuario 
 # no introduce un valor numérico del que no se pueda calcular su factorial.
+
+# 6! = 6 * 5 * 4 * 3 * 2 * 1 = 1 * 2 * 3 * 4 * 5 * 6
+
+res = 1 # es factorial de 0
+factorial = int(input("Introduce un número para calcular su factorial: "))
+i = 1
+
+if factorial < 0:
+    print("Factorial de numero negativo")
+    exit()
+while i <= factorial:
+    res = res * i
+    i += 1
+print(res)
--- ej09.py
+++ ej09.py
@@ -5,5 +5,26 @@
 # en el cual se realizan los siguientes pasos:
 # 1 - Divide el número mayor por el número menor
 # 2 - Si la división es exacta, el divisor es el MCD
-# 3 - Si la división no es exacta, divide el divisor entre el resto obtenido y continúa hasta obtener una división exacta. El último divisor obtenido será el MCD
+# 3 - Si la división no es exacta, divide el divisor entre el resto obtenido y continúa hasta 
+# obtener una división exacta. El último divisor obtenido será el MCD
 # El script deberá interrumpirse si el usuario introduce un número negativo.
+
+num1 = int(input("Introduce un número "))
+num2 = int(input("Introduce un número "))
+
+if num1 >= num2:
+    dividendo = num1
+    divisor = num2
+else:
+    dividendo = num2
+    divisor = num1
+
+resto = 1
+
+while resto != 0:
+    resto = dividendo % divisor
+    if resto  == 0:
+        print("MCD es",divisor)
+    else:
+        dividendo = divisor
+        divisor = resto
